877f7368e9ffaca07d3a3d485b057c2fee8f6bd6
z465442774 <465442774@qq.com>
2015-12-24 20:24:47 +0800

    Rock
    github

---
+++ Rock/ViewController.m
@@ -0,0 +1,40 @@
+//
+//  ViewController.m
+//  Rock
+//
+//  Created by mac on 15/12/24.
+//  Copyright © 2015年 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+/*
+    《反恐精英》将玩家分为“反恐精英”（Counter Terrorists）阵营与“恐怖份子”（Terrorists）阵营两队，每个队伍必须在一个地图上进行多回合的战斗。赢得回合的方法是达到该地图要求的目标，或者是完全消灭敌方玩家。
+    游戏共有6种模式，“爆破模式”（Bombplant&Defuse）、“人质救援模式”（Hostage Rescue）、“刺杀模式”（Assassination）、“逃亡模式”（Escape），后期增加了“军火库模式”。其中《反恐精英：零点行动》有剧情任务。
+    游戏提供了4种不同的人物皮肤，在《反恐精英：零点行动》中增加了两种。而在《反恐精英：全球攻势》中则不能选择人物皮肤，人物的样式会根据玩家所选择的地图的不同而变得不同。
+    金钱系统是游戏的特色系统，因此《反恐精英》在游戏时除了需要玩家的游戏技术外，往往还要有一定的经济规划能力。游戏的绝大多数内容成为之后其他射击游戏争相模仿的要素。
+    《反恐精英》按照创始人杰西·克利夫（Jess Cliffe）的说法，反恐精英——
+    “是基于团队起主要作用的游戏，一队扮演恐怖分子的角色，另一队扮演反恐精英的角色。每一边能够使用不同的枪支、装备，这些枪支和装备具有不同的作用。地图有不同的目标：援救人质，暗杀，拆除炸弹，逃亡等等”。”
+    这款游戏是在1999年夏天诞生的，创始人是杰斯·克利夫和“鹅人”李明（Minh Gooseman Lee）。他们后来和游戏一起进入了Valve。
+*/
+/*
+ 《英雄联盟》（简称lol）是由美国Riot Games开发，腾讯游戏运营的英雄对战网游。《英雄联盟》除了即时战略、团队作战外，还拥有特色的英雄、自动匹配的战网平台，包括天赋树、召唤师系统、符文等元素。
+ 
+ */
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
